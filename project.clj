f624529ec3f225ecf80de41b42d5406cc88fcdc8
Philip S Doctor <pdoctor@opinionlab.com>
2014-01-18 16:54:01 -0600

    Recompiled js

--- project.clj
+++ project.clj
@@ -7,7 +7,9 @@
   :source-paths ["src/clj"]
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [prismatic/dommy "0.1.1"]]
+                 [org.clojure/clojurescript "0.0-1934"]
+                 [prismatic/dommy "0.1.1"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]]
 
   :plugins [[lein-cljsbuild "0.3.2"]]
 
--- src/cljs/phil_and_amber/app.cljs
+++ src/cljs/phil_and_amber/app.cljs
@@ -1,6 +1,7 @@
 (ns phil-and-amber.app
   (:require [dommy.core :refer [replace-contents! listen! attr]]
-            [phil-and-amber.page-templates :refer [app-template]])
+            [phil-and-amber.page-templates :refer [app-template]]
+            [cljs.core.async :as async :refer [<! >! chan]])
   (:require-macros [dommy.macros :refer [node sel1 sel]]))
 
 (defn lang-dispatch
@@ -8,6 +9,7 @@
   (attr element "data-lang-name"))
 
 (defn render-app [container]
-  (replace-contents! container (app-template))
-  (doseq [item (sel :.language)]
-    (listen! item :click (partial lang-dispatch item))))
+  (let [nav-chan (chan)]
+    (replace-contents! container (app-template))
+    (doseq [item (sel :.language)]
+      (listen! item :click (partial lang-dispatch item)))))
